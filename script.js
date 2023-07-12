11ab3da8f96c961bb583291c515186c35082f415
Jacarandev <131291493+Jacarandev@users.noreply.github.com>
2023-07-12 10:51:22 +0200

    Add game function and refreshing results of the game

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let array = ['Rock', 'Paper', 'Scissors'];
+let playerWins = 0;
+let compWins = 0;
 
 function getComputerChoice(){
     let randInt = Math.floor(Math.random() * array.length);
@@ -11,35 +13,53 @@ function playRound(playerSelection, computerSelection){
     computerSelection = computerSelection.toUpperCase();
     if (playerSelection === 'ROCK') {
         if (computerSelection === 'SCISSORS'){
-            return ('You win! Rock beats scissors');
+            alert ('You win! Rock beats scissors');
+            playerWins += 1;
         } else if (computerSelection === 'PAPER') {
-            return ('You lose! Paper beats rock');
+            alert ('You lose! Paper beats rock');
+            compWins += 1;
         } else {
-            return ('It is a tie!');
+            alert ('It is a tie!');
         }
     }
     if (playerSelection === 'PAPER') {
         if (computerSelection === 'ROCK') {
-            return ('You win! Paper beats rock');
+            alert ('You win! Paper beats rock');
+            playerWins += 1;
         } else if (computerSelection === 'SCISSORS') {
-            return ('You lose! Scissors beat paper');
+            alert ('You lose! Scissors beat paper');
+            compWins += 1;
         } else {
-            return ('It is a tie!');
+            alert ('It is a tie!');
         }
     }
     if (playerSelection === "SCISSORS") {
         if (computerSelection === 'PAPER') {
-            return ('You win! Scissors beat paper');
+            alert ('You win! Scissors beat paper');
+            playerWins += 1;
         } else if (computerSelection === 'ROCK') {
-            return ('You lose! Rock beats scissors');
+            alert ('You lose! Rock beats scissors');
+            compWins += 1;
         } else {
-            return ('It is a tie!');
+            alert ('It is a tie!');
         }
     }
 }
 
-let playerSelection = "rock";
+function game () {
+    let numGames = 0;
+    while (numGames < 5) {
+        let playerSelection = prompt('Please enter an option to play: ');
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection)
+        numGames++;
+        alert('Computer: ' + compWins + ' Player: ' + playerWins);
+    }
+    alert('Game finished!');
+}
+/*let playerSelection = prompt('Please enter an option to play: ');
 let computerSelection = getComputerChoice();
 console.log(playerSelection);
 console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));*/
+console.log(game())
